f4af9361a043a69bbdcd5fef405963608a88c87b
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2021-08-27 02:40:05 +0800

    add echarts

---
+++ dist/js/chunk-2d0e5f4f.a8a6c7df.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5f4f"],{9761:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("example",[n("xdh-map",{attrs:{zoom:8}},[n("xdh-map-rectangle",{attrs:{position:[120,30],fill:"#f00",width:1,height:1,text:"文字文字",color:"#fff",font:"20px"}})],1)],1)],1)},o=[],f={},i=f,l=n("2877"),r=Object(l["a"])(i,a,o,!1,null,"d556499a",null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0e5f4f.a8a6c7df.js.map
