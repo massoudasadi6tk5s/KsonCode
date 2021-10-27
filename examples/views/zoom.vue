e4f61f617056450f760d5711bc28efa9008036ef
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2021-10-27 09:04:06 +0800

    '代码优化'

--- examples/views/zoom.vue
+++ examples/views/zoom.vue
@@ -5,7 +5,6 @@
         <xdh-map-zoom placement="left-top" :simple="false"></xdh-map-zoom>
       </xdh-map>
     </example>
-    <div class="test"></div>
   </div>
 </template>
 
@@ -27,15 +26,4 @@
 </script>
 
 <style scoped>
-.test{
-  width: 20px;
-  height: 280px;
-  border: 1px solid red;
-  background: no-repeat;
-  background-image: repeating-linear-gradient(black 0, black 1px, transparent 0, transparent 3px),
-                    repeating-linear-gradient(black 0, black 1px, transparent 0, transparent 15px);
-  background-size: 3px 100%, 5px 100%;
-  background-position: center top;
-
-}
 </style>
