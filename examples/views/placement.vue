82658631130a8499a103c9a6d29e6ac170191b7f
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2021-11-13 09:51:06 +0800

    add icons

--- examples/views/placement.vue
+++ examples/views/placement.vue
@@ -12,6 +12,30 @@
         <div style="padding: 10px">深色容器</div>
       </xdh-map-placement>
 
+      <xdh-map-placement placement="center-top" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">center-top</div>
+      </xdh-map-placement>
+
+      <xdh-map-placement placement="center-center" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">center-center</div>
+      </xdh-map-placement>
+
+      <xdh-map-placement placement="center-bottom" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">center-bottom</div>
+      </xdh-map-placement>
+
+      <xdh-map-placement placement="right-top" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">right-top</div>
+      </xdh-map-placement>
+
+      <xdh-map-placement placement="right-center" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">right-center</div>
+      </xdh-map-placement>
+
+      <xdh-map-placement placement="right-bottom" :margin="[10, 10]" theme="light">
+        <div style="padding: 10px">right-bottom</div>
+      </xdh-map-placement>
+
     </xdh-map>
   </example>
 </template>
