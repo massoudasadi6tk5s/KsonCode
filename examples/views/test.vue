4c38d32088c3460308704a4059e012e24467cb29
massoudasadi6tk5s <yuanpaodengdu4689@163.com>
2022-06-13 21:23:58 +0800

    '修改test页面'

--- examples/views/test.vue
+++ examples/views/test.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-19 21:57:21
- * @LastEditTime: 2019-09-08 18:41:25
+ * @LastEditTime: 2019-09-14 13:13:18
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -11,7 +11,7 @@
         <div ref="thing" draggable="true" style="width: 50px; height: 50px; background: red"></div>
       </div>
       <div class="map-warp" style="width: 100%; height: 500px;">
-        <xdh-map v-droppable :zoom="16" :center="[120.2014, 30.2574]" ref="map" >
+        <xdh-map :zoom="16" :center="[120.2014, 30.2574]" ref="map" >
         
         </xdh-map>
       </div>
@@ -21,9 +21,52 @@
 
  
 <script>
-import droppable from '../../packages/tools/dragdrop/index'
+
+// import {defaults as defaultInteractions, Pointer as PointerInteraction} from 'ol/interaction.js'
+// const Drag = (function (PointerInteraction) {
+//   function Drag() {
+//     PointerInteraction.call(this, {
+//       handleDownEvent: handleDownEvent,
+//       handleDragEvent: handleDragEvent,
+//       handleMoveEvent: handleMoveEvent,
+//       handleUpEvent: handleUpEvent
+//     });
+
+//     /**
+//      * @type {module:ol/pixel~Pixel}
+//      * @private
+//      */
+//     this.coordinate_ = null;
+
+//     /**
+//      * @type {string|undefined}
+//      * @private
+//      */
+//     this.cursor_ = 'pointer';
+
+//     /**
+//      * @type {module:ol/Feature~Feature}
+//      * @private
+//      */
+//     this.feature_ = null;
+
+//     /**
+//      * @type {string|undefined}
+//      * @private
+//      */
+//     this.previousCursor_ = undefined;
+//   }
+
+//   if (PointerInteraction) Drag.__proto__ = PointerInteraction;
+//   Drag.prototype = Object.create( PointerInteraction && PointerInteraction.prototype );
+//   Drag.prototype.constructor = Drag;
+
+//   return Drag;
+// }(PointerInteraction))
+
+
 export default {
-  directives: {droppable},
+  directives: {},
   data() {
     return {
     }
